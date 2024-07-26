4e080c0a06e3f703fa30c43e5a944ee7827208df
Lexie <m18140663022_1@163.com>
2024-07-25 21:29:24 -0700

    11

--- src/Exercise21.java
+++ src/Exercise21.java
@@ -20,25 +20,47 @@ public class Exercise21 {
 
     class Solution {
         public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-            ListNode first = list1;
-            ListNode second = list2;
-            ListNode prev;
-            if (first.val < second.val) {
-                while (first.val < second.val) {
-                    prev = first;
-                    first = first.next;
-                }
-            } else {
-                while (second.val <= first.val) {
-                    prev = second;
-                    second = second.next;
+            ListNode first = new ListNode();
+            ListNode prev = first;
+            if (list1 == null && list2 == null)
+                return null;
+            while (list1 != null && list2 != null) {
 
+                if (list1.val < list2.val) {
+                    prev.next = list1;
+                    list1 = list1.next;
+                } else {
+                    prev.next = list2;
+                    list2 = list2.next;
                 }
-
+                prev = prev.next;
             }
+            if (list1 == null)
+                prev.next = list2;
+            else
+                prev.next = list1;
 
-            return first;
-
+            return first.next;
         }
+
     }
+
+    public static void main(String[] args) {
+
+        Exercise21 exercise = new Exercise21();
+        Solution solution = exercise.new Solution();
+
+        ListNode list1_3 = exercise.new ListNode(4);
+        ListNode list1_2 = exercise.new ListNode(2, list1_3);
+        ListNode list1_1 = exercise.new ListNode(1, list1_2);
+
+        ListNode list2_3 = exercise.new ListNode(4);
+        ListNode list2_2 = exercise.new ListNode(3, list2_3);
+        ListNode list2_1 = exercise.new ListNode(1, list2_2);
+
+        ListNode first = solution.mergeTwoLists(list1_1, list2_1);
+        for (; first != null; first = first.next)
+            System.out.println(first.val);
+    }
+
 }
