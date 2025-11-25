f1fe1f50efd26279d5726397c2a92b64d2a73e33
Lexie <m18140663022_1@163.com>
2025-11-25 11:13:35 -0800

    Exercise136

---
+++ src/StringAndArray/Exercise136.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int ans = 0;
+        for (auto num : nums)
+        {
+            ans = ans ^ num;
+        }
+        return ans;
+    };
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums {4, 1, 2, 1, 2};
+    cout << s.singleNumber(nums) << endl; // Output: 4
+    return 0;
+}
